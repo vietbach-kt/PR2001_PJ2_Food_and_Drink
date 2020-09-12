5943b48435b8df422e89c33286634ac06d09d481
Your Name <truongvietbach10a1k64@gmail.com>
2020-09-12 14:05:31 +0700

    change kind

--- app/models/topping.rb
+++ app/models/topping.rb
@@ -1,7 +1,8 @@
+
 class Topping < ApplicationRecord
   belongs_to :product
 
-  enum type: {food: 0, drink: 1}
+  enum kind: {food: 0, drink: 1}
 
   validates :name, presence: true
   validates :price, presence: true
--- db/migrate/20200825072055_create_products.rb
+++ db/migrate/20200825072055_create_products.rb
@@ -5,7 +5,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.text :information
       t.integer :old_price
       t.integer :price
-      t.integer :kind
+      t.integer :kind, index:true
       t.references :category, null: false, foreign_key: true
       t.timestamps
     end
