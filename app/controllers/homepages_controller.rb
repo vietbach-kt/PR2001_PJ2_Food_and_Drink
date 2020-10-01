a7687c912970f77e42b39d650e6ce28b31946915
Your Name <truongvietbach10a1k64@gmail.com>
2020-10-01 21:52:06 +0700

    task 2

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -3,5 +3,6 @@ class HomepagesController < ApplicationController
     @categories = Category.all
     @products = Product.all
     @carts = current_cart
+    @addresses = current_user.addresses
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,4 +10,19 @@ class Cart < ApplicationRecord
       ci.valid? ? ci.quantity * ci.save_price_to_cart_item : 0
     end
   end
+  def coupons
+    a = self.caculate_total_amount.sum.to_f
+    if a >= 100
+      a * 10/100
+    elsif a >= 80
+      a * 5/100
+    elsif a >= 35
+      a * 2/100
+    else
+      a
+    end
+  end
+  def total_price_after_coupons
+    b = self.caculate_total_amount.sum.to_f - self.coupons
+  end
 end
